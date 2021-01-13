feddc5cfdb2ddd9d239decccaba992bbfb44a7ae
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-12 22:24:53 -0500

    commit tree 6ea2ff6e0f77fc5deb712c57490defa260d74b9f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+let dodger = document.getElementById("dodger")
+
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  } 
+
+})
+
+
+function moveDodgerLeft() {
+  let leftNums = parseInt(dodger.style.left);
+  dodger.style.left = `${leftNums - 1}px`;
+}
+
+function moveDodgerRight() {
+  let leftNums = parseInt(dodger.style.left);
+  dodger.style.left = `${leftNums + 1}px`;
+}
